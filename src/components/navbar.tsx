3d185a8f51192fd0134ed4603f07d3e94032b1f9
Akshat Jaiswal <akshatjaiswal.official@gmail.com>
2026-03-21 17:30:09 +0530

    refactor (layout): change div to main for semantic structure refactor (navbar): format Image component props for readability
    Signed-off-by: Akshat Jaiswal <akshatjaiswal.official@gmail.com>

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -20,7 +20,13 @@ export const Navbar = React.memo(() => {
     <section className='fixed top-0 z-50 w-full bg-transparent backdrop-blur-sm'>
       <div className='xs:px-4 container flex w-full items-center justify-between py-4 sm:px-8'>
         <Link href='/' className='flex animate-pulse cursor-pointer items-center'>
-          <Image src={logo} className='mr-2 h-8 w-10 rounded-lg' alt='Body Works logo' />
+          <Image
+            src={logo}
+            className='mr-2 h-8 w-10 rounded-lg'
+            alt='Body Works logo'
+            priority
+            quality={80}
+          />
           {!showLogoText && (
             <p className='font-poppins bg-linear-to-r from-amber-600 to-amber-500 bg-clip-text text-xl font-bold text-transparent transition-all duration-300 ease-in-out dark:bg-linear-to-r dark:from-pink-500 dark:to-violet-500'>
               Works
